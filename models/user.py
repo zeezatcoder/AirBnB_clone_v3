6a498e2b2b1776dfd003cfbd2c98affd845dc935
frank-gich <gichuhi5781@gmail.com>
2023-01-23 16:53:27 +0300

    chek fix

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ holds class User"""
-import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -17,8 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete, delete-orphan", backref="user")
-        reviews = relationship("Review", cascade="all, delete, delete-orphan", backref="user")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
@@ -28,12 +28,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-    def __setattr__(self, __name: str, __value) -> None:
-        '''Sets an attribute of this class to a given value.'''
-        if __name == 'password':
-            if type(__value) is str:
-                """Hash password using md5"""
-                m = hashlib.md5(bytes(__value, 'utf-8'))
-                super().__setattr__(__name, m.hexdigest())
-        else:
-            super().__setattr__(__name, __value)
+
+    def __setattr__(self, name, value):
+        """ Override setattr to md5 hash `value` if `name` is password """
+        if name == "password":
+            value = hashlib.md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
